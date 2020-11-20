4185e2578325891dbb12a390340140cc9b55d31e
Sergio D. Barrera <sdbeng@gmail.com>
2020-11-20 08:38:12 -0800

    refactor conditionals

--- src/components/Image.js
+++ src/components/Image.js
@@ -7,21 +7,28 @@ function Image(props){
     //next, track hover state
     //create state boolean for hovered
     const [hovered, setHovered] = useState(false)
-    console.log({hovered})
+    // console.log({hovered})
 
     function handleHover(){
         // console.log('on mouse over')
         setHovered(true)
+
     }
+
+    // conditionally render the heart and plus icon when an Image component is being hovered on - create 2 variables
+    const heartIcon = hovered && <a href="/"><i className="ri-heart-line favorite"></i></a>
+    const cartIcon = hovered && <a href="/"><i className="ri-add-circle-line cart"></i></a>
 
     return (
         <div className={`${props.someClass} image-container`}>
             <img 
-                src={props.photo.url}
+                src={props.photo.url}                
                 className="image-grid"                
                 onMouseEnter={handleHover}
                 onMouseLeave={() => setHovered(false)}
                 alt=""/>
+                {heartIcon}
+                {cartIcon}
         </div>
     )
 }
